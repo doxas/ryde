6ebcd0f6d7cf77d3b25286f6a1601e535792e33d
doxas <eizanco@gmail.com>
2022-04-17 10:08:55 +0900

    setup layout

--- web/component/Layout/Layout.tsx
+++ web/component/Layout/Layout.tsx
@@ -1,17 +1,19 @@
-/* eslint-disable @next/next/no-img-element */
 
-import {useState, useEffect, useRef} from 'react';
 import styles from './Layout.module.scss';
 
 type LayoutProps = {
   children: React.ReactNode;
+  layer: React.ReactNode;
 };
 
-function Layout({children}: LayoutProps): JSX.Element {
+function Layout({children, layer}: LayoutProps): JSX.Element {
   return (
     <>
+      <div className={styles.wrapper}>
+        {children}
+      </div>
       <div className={styles.layer}>
-        hoge
+        {layer}
       </div>
     </>
   );
