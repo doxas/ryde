a4043dea9844e26d7d53ed5ae6b3ba3fefd37a06
doxas <eizanco@gmail.com>
2022-04-22 17:16:11 +0900

    move useEffect from layout to index

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,22 +1,42 @@
 
+import {useState, useEffect} from 'react';
 import type {NextPage} from 'next';
 
 import Meta from '../component/Meta/Meta';
 import Layout from '../component/Layout/Layout';
 
 const Home: NextPage = () => {
+  const [dark, setDark] = useState<Boolean>(false);
+
+  useEffect(() => {
+    const darken = localStorage.getItem('dark');
+    const isDark = (darken === 'true');
+    setDark(isDark);
+
+    window.addEventListener('keydown', () => {
+      const isDark = !(dark === true);
+      setDark(isDark);
+      localStorage.setItem('dark', `${isDark}`);
+    }, {once: true});
+  }, []);
+
+  const wrap = (
+    <>{dark ? 'dark' : 'light'}</>
+  );
+  const layer = (
+    <>hoge</>
+  );
+
   return (
     <>
       <Meta
         title={'ryde'}
         description={'ryde - live'}
       />
-      <div className="full">
-        <Layout
-          children="hoge"
-          layer="fuga"
-        />
-      </div>
+      <Layout
+        children={wrap}
+        layer={layer}
+      />
     </>
   );
 };
